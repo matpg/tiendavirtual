52c4b3d3fdce716903ba9e8fd8cf30d0f2de30e2
matpg <matpoga@gmail.com>
2021-09-24 14:41:27 -0300

    carpeta general agregada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './general/header/header.component';
+import { FooterComponent } from './general/footer/footer.component';
+import { AsideComponent } from './general/aside/aside.component';
+import { CategoriaComponent } from './general/categoria/categoria.component';
+import { ProductoComponent } from './general/producto/producto.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AsideComponent,
+    CategoriaComponent,
+    ProductoComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
