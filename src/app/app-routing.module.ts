69f8ec32134da2290c449562f19399a9f0877ebc
matpg <matpoga@gmail.com>
2021-09-24 18:02:20 -0300

    nuevos componentes agregados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,25 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './general/home/home.component';
+import { Pagina404Component } from './general/pagina404/pagina404.component';
 import { ProductoComponent } from './general/producto/producto.component';
-import { ProductoComponent } from './general/login/login.component';
-import { ProductoComponent } from './general/categoria/categoria.component';
-import { ProductoComponent } from './general/registro/registro.component';
+import { RegistroComponent } from './general/registro/registro.component';
 
-// referencias a los componentes
+
+// referencias a los componentes 
+// path 'nombreruta', nombre componente
 const app_routes: Routes = [
-    { path: '', component: ProductoComponent},
-    { path: '', component: LoginComponent},
-    { path: '', component: CategoriaComponent},
-    { path: '', component: RegistroComponent},
+    { path: '', component: HomeComponent }, //la main
+    { path: 'producto', component: ProductoComponent },
+    { path: 'registro', component: RegistroComponent},
+    { path:'**', component: Pagina404Component}, //default
 
 ];
-// Decorador: no olvidar
+
+//decorador: no olvidar importarlo
 @NgModule({
-    imports: [RouterModule.forRoot(),]
+    imports: [RouterModule.forRoot(app_routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,39 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+//rutas
+import { AppRoutingModule } from './app-routing.module';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './general/header/header.component';
+import { AsideComponent } from './general/aside/aside.component';
+import { FooterComponent } from './general/footer/footer.component';
+import { ProductoComponent } from './general/producto/producto.component';
+import { CategoriaComponent } from './general/categoria/categoria.component';
+import { LoginComponent } from './general/login/login.component';
+import { RegistroComponent } from './general/registro/registro.component';
 import { HomeComponent } from './general/home/home.component';
 import { Pagina404Component } from './general/pagina404/pagina404.component';
-import { ProductoComponent } from './general/producto/producto.component';
-import { RegistroComponent } from './general/registro/registro.component';
 
-
-// referencias a los componentes 
-// path 'nombreruta', nombre componente
-const app_routes: Routes = [
-    { path: '', component: HomeComponent }, //la main
-    { path: 'producto', component: ProductoComponent },
-    { path: 'registrarse', component: RegistrarseComponent},
-    { path:'**', component: Pagina404Component}, //default
-
-];
-
-//decorador: no olvidar importarlo
 @NgModule({
-    imports: [RouterModule.forRoot(app_routes)],
-    exports: [RouterModule]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        AsideComponent,
+        FooterComponent,
+        ProductoComponent,
+        CategoriaComponent,
+        LoginComponent,
+        RegistroComponent,
+        HomeComponent,
+        Pagina404Component
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-
-export class AppRoutingModule { }
+export class AppModule { }
